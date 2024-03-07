d2d73b5f017596f10e53b541e5afa604d294ad46
LealO-O <86996329+LealO-O@users.noreply.github.com>
2024-03-07 10:45:45 +0800

    Update __init__.py

--- TopMost-main/topmost/trainers/__init__.py
+++ TopMost-main/topmost/trainers/__init__.py
@@ -3,11 +3,3 @@ from .basic.LDA_trainer import LDAGensimTrainer
 from .basic.LDA_trainer import LDASklearnTrainer
 from .basic.NMF_trainer import NMFGensimTrainer
 from .basic.NMF_trainer import NMFSklearnTrainer
-
-from .crosslingual.crosslingual_trainer import CrosslingualTrainer
-from .dynamic.dynamic_trainer import DynamicTrainer
-
-from .dynamic.DTM_trainer import DTMTrainer
-
-from .hierarchical.hierarchical_trainer import HierarchicalTrainer
-from .hierarchical.HDP_trainer import HDPGensimTrainer
